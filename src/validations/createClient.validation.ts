9b6a623a72a5deae581e7aea0a58c2785c1db133
Renan Goulart <renan.goulart4@gmail.com>
2024-03-07 00:21:27 -0300

    fix: camelCase in create client fields

--- src/validations/createClient.validation.ts
+++ src/validations/createClient.validation.ts
@@ -5,16 +5,16 @@ export type CreateClientForm = Yup.InferType<typeof CreateClientSchema>;
 export const CreateClientSchema = Yup.object({
   name: Yup.string().required('Nome Obrigatório'),
   gender: Yup.string().required('Gênero Obrigatório'),
-  birth_date: Yup.string().required('Data de Nascimento Obrigatória'),
+  birthDate: Yup.string().required('Data de Nascimento Obrigatória'),
   cpf: Yup.string().required('CPF Obrigatório'),
   ddd: Yup.string().required('DDD Obrigatório'),
   phone: Yup.string().required('Telefone Obrigatório'),
-  phone_type: Yup.string().required('Tipo de Telefone Obrigatório'),
+  phoneType: Yup.string().required('Tipo de Telefone Obrigatório'),
   email: Yup.string().email('Email Inválido').required('Email Obrigatório'),
   password: Yup.string()
     .min(8, 'A senha deve ter ao menos 8 dígitos')
     .required('Senha Obrigatória'),
-  confirm_password: Yup.string()
+  confirmPassword: Yup.string()
     .oneOf([Yup.ref('password')], 'A senha de confirmação não confere')
     .required('Confirmação de senha obrigatória'),
 });
