5c439a405c5bf6b6dd46616ba117d81d51dc8f86
Renan Goulart <renan.goulart4@gmail.com>
2024-05-27 01:16:55 -0300

    fix: error message on home

--- src/components/ProductsGrid/ProductsGrid.tsx
+++ src/components/ProductsGrid/ProductsGrid.tsx
@@ -34,10 +34,7 @@ export const ProductsGrid = ({ category }: Props) => {
   }, [products, category]);
 
   const renderErrorMessage = () => {
-    if (
-      (category && filteredProducts?.length === 0) ||
-      products?.length === 0
-    ) {
+    if ((category && filteredProducts?.length === 0) || !products) {
       return (
         <NotFoundContainer>
           <NotFoundText>Nenhum produto encontrado...😭</NotFoundText>
@@ -52,13 +49,13 @@ export const ProductsGrid = ({ category }: Props) => {
     }
     if (category) {
       return filteredProducts.map((product) =>
-        product.quantityInStock > 0 && product.status=="ATIVO" ? (
+        product.quantityInStock > 0 && product.status == "ATIVO" ? (
           <ProductCard key={product.id} data={product} />
         ) : null,
       );
     }
     return products.map((product) =>
-      product.quantityInStock > 0 && product.status=="ATIVO" ? (
+      product.quantityInStock > 0 && product.status == "ATIVO" ? (
         <ProductCard key={product.id} data={product} />
       ) : null,
     );
